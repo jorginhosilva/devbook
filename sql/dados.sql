dad3f9d3cdbdb1faa4532b5341993ea01bdcc7dd
André Luiz <ma650696@gmail.com>
2024-09-08 08:51:09 -0300

    criação de tabela de seguidores

---
+++ sql/dados.sql
@@ -0,0 +1,5 @@
+insert into usuarios (nome, nick, email, senha)
+values
+("Usuário 1", "usuario_1", "usuario1@gmail.com", "aqui vai o token"), -- usuario1
+("Usuário 2", "usuario_2", "usuario2@gmail.com", "aqui vai o token"), -- usuario2
+("Usuário 3", "usuario_3", "usuario3@gmail.com", "aqui vai o token"), -- usuario3
