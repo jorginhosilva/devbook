8efbe2e3b4159dbfec2662e1088e1d2902a31987
André Luiz <ma650696@gmail.com>
2024-09-09 05:34:42 -0300

    tentando descobrir um erro

--- sql/dados.sql
+++ sql/dados.sql
@@ -1,5 +1,11 @@
 insert into usuarios (nome, nick, email, senha)
 values
-("Usuário 1", "usuario_1", "usuario1@gmail.com", "aqui vai o token"), -- usuario1
-("Usuário 2", "usuario_2", "usuario2@gmail.com", "aqui vai o token"), -- usuario2
-("Usuário 3", "usuario_3", "usuario3@gmail.com", "aqui vai o token"), -- usuario3
+("Usuário 1", "usuario_1", "usuario1@gmail.com", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MjU4OTE1MjEsInVzdWFyaW9JZCI6MTR9.BGYa-1kDs64OoY5U8OvERIB1Qxdwqed3kI5uLMG9ynY"), -- usuario1
+("Usuário 2", "usuario_2", "usuario2@gmail.com", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MjU4OTE1MjEsInVzdWFyaW9JZCI6MTR9.BGYa-1kDs64OoY5U8OvERIB1Qxdwqed3kI5uLMG9ynY"), -- usuario2
+("Usuário 3", "usuario_3", "usuario3@gmail.com", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MjU4OTE1MjEsInVzdWFyaW9JZCI6MTR9.BGYa-1kDs64OoY5U8OvERIB1Qxdwqed3kI5uLMG9ynY"), -- usuario3
+
+insert into seguidores(usuario_id, seguidor_id)
+values
+(1, 2),
+(3, 1),
+(1, 3);
